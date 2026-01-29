ddea54377a26c7dc732b18e7ba279353a75943c9
Purice <andrei.purice1203@gmail.com>
2026-01-29 15:11:19 +0200

    First Commit

---
+++ src/main/java/com/businesstools/flowstock/FlowStockApplication.java
@@ -0,0 +1,13 @@
+package com.businesstools.flowstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/businesstools/flowstock/FlowstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businesstools.flowstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
