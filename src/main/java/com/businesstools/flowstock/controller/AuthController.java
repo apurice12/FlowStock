f37d60b72ddd319ca7c990cb729d9f618b3038e8
andreipu <andrei.purice@simavi.ro>
2026-01-31 21:38:42 +0200

    landing page UI

---
+++ src/main/java/com/businesstools/flowstock/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.businesstools.flowstock.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login-page/login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "/login-page/login";
+    }
+
+    @GetMapping("/landing-page")
+    public String landingPage() {
+        return "landing-page/landing-page";
+    }
+}
